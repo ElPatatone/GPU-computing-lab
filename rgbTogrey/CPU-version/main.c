2a2d95b834248beee05faeffb95f3958c126f19a
ElPatatone <rahman.hm2002@gmail.com>
2025-08-07 20:34:41 +0100

    feat: add greyscale kernel + comparison between the cpu and gpu

--- rgbTogrey/CPU-version/main.c
+++ rgbTogrey/CPU-version/main.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 
-#define WIDTH 2
-#define HEIGHT 2
+#define WIDTH 32
+#define HEIGHT 32
 #define IMAGE_SIZE (WIDTH * HEIGHT)
 
 void createImage(FILE* fptr, unsigned char* output) {
@@ -10,7 +10,7 @@ void createImage(FILE* fptr, unsigned char* output) {
     fprintf(fptr, "%d %d\n", WIDTH, HEIGHT);
     fprintf(fptr, "255\n");
 
-    for (int i = 0; i < WIDTH * HEIGHT; i++) {
+    for (int i = 0; i < IMAGE_SIZE; i++) {
         fprintf(fptr, "%d ", output[i]);
         if ((i + 1) % WIDTH == 0) fprintf(fptr, "\n");
     }
@@ -18,15 +18,24 @@ void createImage(FILE* fptr, unsigned char* output) {
     fclose(fptr);
 }
 
+void writePPM(FILE* fptr, unsigned char* rgb) {
+    fprintf(fptr, "P6\n");
+    fprintf(fptr, "%d %d\n", WIDTH, HEIGHT);
+    fprintf(fptr, "255\n");
+
+    fwrite(rgb, sizeof(unsigned char), WIDTH * HEIGHT * 3, fptr); 
+    fclose(fptr);
+}
+
 int main(int argc, char *argv[]) {
 
-    unsigned char rgb[IMAGE_SIZE*3] = { 255, 0, 0,
-                                        0, 255, 0,
-                                        0, 0, 255,
-                                        255, 255, 255 };
+    // interleaved RGB format unsigned char rgb[IMAGE_SIZE*3] = { 255, 0, 0,
+    //                                     0, 255, 0,
+    //                                     0, 0, 255,
+    //                                     255, 255, 255 };
 
 
-
+    // planar RGB format
     unsigned char r[IMAGE_SIZE] = {255, 0, 0, 255};
     unsigned char g[IMAGE_SIZE] = {0, 255, 0, 255};
     unsigned char b[IMAGE_SIZE] = {0, 0, 255, 255};
@@ -34,13 +43,21 @@ int main(int argc, char *argv[]) {
     unsigned char greyPlanar[IMAGE_SIZE];
     unsigned char greyInterleaved[IMAGE_SIZE];
     
-    // planar RGB
+    // planar RGB processing
     for (int i = 0; i < IMAGE_SIZE; i++) {
-        greyPlanar[i] = (int)(r[i]*0.299 + g[i]*0.587 + b[i]*0.114);
+        greyPlanar[i] = (unsigned char)(r[i]*0.299 + g[i]*0.587 + b[i]*0.114);
         printf("Greyscale value for index [%d] =  %d\n", i, greyPlanar[i]);
     }
 
-    // interleaved RGB
+    unsigned char rgb[IMAGE_SIZE*3];
+
+    for (int i = 0; i < IMAGE_SIZE; i++) {
+        rgb[i*3] = i % 256;
+        rgb[i*3 + 1] = (i*2) % 256;
+        rgb[i*3 + 2] = (i*3) % 256;
+    }
+    
+    // interleaved RGB processing
     for (int i = 0; i < IMAGE_SIZE; i++) {
         int offset = i * 3;
         unsigned char r = rgb[offset];
@@ -51,14 +68,18 @@ int main(int argc, char *argv[]) {
         printf("Greyscale value for index [%d] =  %d\n", i, greyInterleaved[i]);
     }
 
-    FILE *fptr1;
-    FILE *fptr2;
-    fptr1 = fopen("greyPlanar.pgm", "w");
-    fptr2 = fopen("greyInterleaved.pgm", "w");
+    FILE *fptr1 = fopen("greyPlanar.pgm", "w");
+    FILE *fptr2 = fopen("greyInterleaved.pgm", "w");
+    FILE *fptr3 = fopen("inputColor.ppm", "wb");
 
-    createImage(fptr1, greyInterleaved);
-    createImage(fptr2, greyPlanar);
+    if (!fptr1 || !fptr2 || !fptr3) {
+        printf("Failed to open the files");
+        return 1;
+    }
 
+    createImage(fptr1, greyPlanar);
+    createImage(fptr2, greyInterleaved);
+    writePPM(fptr3, rgb);
 
     return 0;
 }
---
+++ rgbTogrey/GPU-version/main.cu
@@ -0,0 +1,117 @@
+#include <time.h>
+#include <stdio.h>
+
+#define WIDTH 2048
+#define HEIGHT 2048 
+#define IMAGE_SIZE (WIDTH * HEIGHT)
+
+__global__ void rgbTogrey_kernel(unsigned char *r, unsigned char *g, 
+                                 unsigned char *b, unsigned char *grey) {
+
+    unsigned int row = blockDim.y * blockIdx.y + threadIdx.y;
+    unsigned int col = blockDim.x * blockIdx.x + threadIdx.x;
+
+    // gets the individual pixel in the image
+    unsigned int i = row*WIDTH + col;
+    // boundary checks
+    if (col < WIDTH && row < HEIGHT) {
+        grey[i] = (unsigned char)(r[i]*0.299 + g[i]*0.587 + b[i]*0.114);
+    }
+}
+
+void rgbTogrey_cpu(unsigned char *r, unsigned char *g,
+                   unsigned char *b, unsigned char *grey) {
+
+    // planar RGB processing
+    for (int i = 0; i < IMAGE_SIZE; i++) {
+        grey[i] = (unsigned char)(r[i]*0.299 + g[i]*0.587 + b[i]*0.114);
+    }
+}
+
+
+void rgbTogrey_gpu(unsigned char *r, unsigned char *g,
+                   unsigned char *b, unsigned char *grey) {
+
+    // 1. allocate gpu memory
+    unsigned char *r_d, *g_d, *b_d, *grey_d;
+    
+    cudaMalloc((void**)&r_d, sizeof(unsigned char)*IMAGE_SIZE);
+    cudaMalloc((void**)&g_d, sizeof(unsigned char)*IMAGE_SIZE);
+    cudaMalloc((void**)&b_d, sizeof(unsigned char)*IMAGE_SIZE);
+    cudaMalloc((void**)&grey_d, sizeof(unsigned char)*IMAGE_SIZE);
+
+    // 2. copy data from cpu to gpu
+    cudaMemcpy(r_d, r, sizeof(unsigned char)*IMAGE_SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(g_d, g, sizeof(unsigned char)*IMAGE_SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(b_d, b, sizeof(unsigned char)*IMAGE_SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(grey_d, grey, sizeof(unsigned char)*IMAGE_SIZE, cudaMemcpyHostToDevice);
+
+    // 3. run the kernel
+    dim3 numThreadsPerBlock(32, 32);
+    dim3 numBlocks((WIDTH + numThreadsPerBlock.x - 1)/numThreadsPerBlock.x, 
+                   (HEIGHT + numThreadsPerBlock.y - 1)/numThreadsPerBlock.y);
+    
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start, 0);
+    rgbTogrey_kernel<<<numBlocks, numThreadsPerBlock>>>(r_d, g_d, b_d, grey_d);
+    cudaEventRecord(stop, 0);
+
+    cudaError_t error = cudaEventSynchronize(stop);
+    if (error != cudaSuccess) {
+        printf("[Error] Kernel failed to launch: %s", cudaGetErrorString(error));
+    }
+
+    float timeTaken = 0;
+    cudaEventElapsedTime(&timeTaken, start, stop);
+    printf("GPU computation took: %.10f seconds\n", timeTaken/1000);
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    // 4. copy data from cpu to gpu
+    cudaMemcpy(grey, grey_d, sizeof(unsigned char)*IMAGE_SIZE, cudaMemcpyDeviceToHost);
+
+    // 5. free the gpu memory
+    cudaFree(r_d);
+    cudaFree(g_d);
+    cudaFree(b_d);
+    cudaFree(grey_d);
+}
+
+int main(int argc, char *argv[]) {
+
+    // unsigned char rgb[IMAGE_SIZE*3];
+    unsigned char *r = (unsigned char*) malloc(IMAGE_SIZE); 
+    unsigned char *g = (unsigned char*) malloc(IMAGE_SIZE); 
+    unsigned char *b = (unsigned char*) malloc(IMAGE_SIZE); 
+
+    for (int i = 0; i < IMAGE_SIZE; i++) {
+        r[i] = i % 256;
+        g[i] = (i*2) % 256;
+        b[i] = (i*3) % 256;
+    }
+
+    // unsigned char r[IMAGE_SIZE] = {255, 0, 0, 255};
+    // unsigned char g[IMAGE_SIZE] = {0, 255, 0, 255};
+    // unsigned char b[IMAGE_SIZE] ={0, 0, 255, 255};
+    unsigned char *greyPlanar = (unsigned char*) malloc(IMAGE_SIZE);
+    
+    rgbTogrey_gpu(r, g, b, greyPlanar);
+
+    clock_t start = clock();
+    rgbTogrey_cpu(r, g, b, greyPlanar);
+    clock_t stop = clock();
+
+    float timeTaken = (float) (stop - start) / CLOCKS_PER_SEC;
+    printf("CPU computation took: %.10f seconds\n", timeTaken);
+
+    free(r);
+    free(g);
+    free(b);
+    free(greyPlanar);
+
+    return 0;
+}
