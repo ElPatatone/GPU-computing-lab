ac6b9e56ff04212b8b3e3c7bad284f95d464026e
ElPatatone <rahman.hm2002@gmail.com>
2025-09-13 23:37:28 +0100

    feat: make helper function for error handling

--- practice/main.cu
+++ practice/main.cu
@@ -4,11 +4,21 @@ __global__ void add(int a, int b, int *c) {
     *c = a + b;
 }
 
+void handleError(cudaError_t error, const char *file, int line) {
+    if (error != cudaSuccess) {
+        printf("Error: %s at line %d in file %s\n", cudaGetErrorString(error), line, file);
+        exit(EXIT_FAILURE);
+    }
+}
+
+#define HANDLE_ERROR(error) (handleError(error, __FILE__, __LINE__))
+
 int main(void) {
     int c;
     int *c_d;
-    cudaMalloc((void**)&c_d, sizeof(int));
-    cudaMemcpy(&c, c_d, sizeof(int), cudaMemcpyDeviceToHost);
+    HANDLE_ERROR(cudaMalloc((void**)&c_d, sizeof(int)));
+    HANDLE_ERROR(cudaMemcpy(&c, c_d, sizeof(int), cudaMemcpyDeviceToHost));
     printf("hello world\n");
+
     return 0;
 }
